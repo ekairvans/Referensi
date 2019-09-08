315aff8fffdd614741deaa2b2eb70d06494fd2e9
ekairvans <irvaneis21@gmail.com>
2019-09-08 17:38:32 +0700

    membuat file mengirimdata.php

---
+++ mengirimdata.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>cara mengirim data menggunakan php</title>
+</head>
+<body>
+	<form action="hasilmengirimdata.php" method="post">
+		username : <input type="text" name="a">
+		<br>
+		password : <input type="password" name="b">
+		<br>
+		<input type="submit" name="">
+		
+	</form>
+</body>
+</html>
