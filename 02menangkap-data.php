424cdabd75ad2b01ac15fe561ef36124d6151818
ekairvans <irvaneis21@gmail.com>
2019-09-08 20:10:45 +0700

    menggunakan php untuk menangkap data

---
+++ 02menangkap-data.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>menangkap data</title>
+</head>
+<body >
+	<form action="02hasil-menangkap-data.php" method="post">
+	<div class="layer1">
+		<div>
+			<h1>Registrasi</h1>
+		</div>
+		<div class="layer2">
+			<input type="text" name="username1" placeholder="Username">
+		</div>
+		<div class="layer2">
+			<input type="password" name="password1" placeholder="Password">
+		</div>
+		
+		
+		<div class="layer2">
+			<input type="submit" name="login" value="Log In" >
+		</div>
+	
+</div>
+
+</form>
+</body>
+</html>
