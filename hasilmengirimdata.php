fead4495216d34c6214aabb73992ce471c7977d5
ekairvans <irvaneis21@gmail.com>
2019-09-08 17:39:13 +0700

    membuat file hasilmengirimdata.php

---
+++ hasilmengirimdata.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>cara menampilkan data</title>
+</head>
+<body>
+	<form method="post">
+		username : <?php echo $_POST['a']?>
+		<br>
+		password : <?php echo $_POST['b']?>
+	</form>
+</body>
+</html>
